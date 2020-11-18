410b0d91e68c486ae0b071a23411b85983b4d4d6
Sung Woo <yashu3lee@gmail.com>
2020-11-18 10:40:20 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: 800,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    "& input": {
+      boxSizing: "border-box",
+      margin: "1rem 0",
+      padding: ".5rem",
+      width: "400px",
+      height: "3rem",
+      fontSize: "1.5rem",
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <h1>Sign in below!</h1>
+      <form className={classes.form} onSubmit={(event) => signIn(event)}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Pasword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input name="submit" id="submit" type="submit" />
+      </form>
     </div>
   );
 }
